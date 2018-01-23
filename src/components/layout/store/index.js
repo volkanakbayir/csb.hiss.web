cab7bf0f338463b579d84fca2c60b9ca2779afb6
volkan <wqlky@hotmail.com>
2018-01-23 19:55:44 +0300

    store config

--- src/components/layout/store/index.js
+++ src/components/layout/store/index.js
@@ -1,10 +1,11 @@
 import getters from './getters'
 import actions from './actions'
 import mutations from './mutations'
+import menuConfig from '@/config/menu-items.json';
 
 let store = {
     state: {
-
+        menuConfig
     },
     actions,
     getters,
